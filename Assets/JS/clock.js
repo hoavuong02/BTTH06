6ffc380a449aa7fc825b18575b29d7d2d3520b7d
TienNguyen79 <Tientech2002@gmail.com>
2022-03-18 10:07:32 +0700

    Chinh sua file oclock

--- Assets/JS/clock.js
+++ Assets/JS/clock.js
@@ -4,29 +4,20 @@ var display = document.querySelector(".display")
 function showTime(){
     var date = new Date();
     var h = date.getHours(); // 0 - 23
-    console.log(h)
     var m = date.getMinutes(); // 0 - 59
     var s = date.getSeconds(); // 0 - 59
-    var session = "AM";
-    
-    if(h == 0){
-        h = 0;
-    }
-    
-    if(h > 12){
-        // h = h - 12;
-        session = "PM";
-    }
-    
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
-    
-    var time = h + ":" + m + ":" + s + " " + session;
-    document.getElementById("MyClockDisplay").innerText = time;
-    document.getElementById("MyClockDisplay").textContent = time;
-    
-    setTimeout(showTime, 1000);
+    var H1 = document.querySelector(".H1")
+    var H2 = document.querySelector(".H2")
+    var M1 = document.querySelector(".M1")
+    var M2 = document.querySelector(".M2")
+    var S1 = document.querySelector(".S1")
+    var S2 = document.querySelector(".S2")
+    H1.src = "./Assets/BTTH_icons/0" + Math.floor(h/10)+ ".gif"
+    H2.src = "./Assets/BTTH_icons/0" + h%10+ ".gif"
+    M1.src = "./Assets/BTTH_icons/0" + Math.floor(m/10)+ ".gif"
+    M2.src = "./Assets/BTTH_icons/0" + m%10+ ".gif"
+    S1.src = "./Assets/BTTH_icons/0" + Math.floor(s/10)+ ".gif"
+    S2.src = "./Assets/BTTH_icons/0" + s%10 + ".gif"
     if(h>=0 && h<12){
         display.innerHTML = "Chào buổi Sáng";
     }
@@ -39,4 +30,14 @@ function showTime(){
 
 }
 
-showTime();
+function blink(){
+    display.style.color = "black";
+    // display.style.fontSize = "20px";
+    setTimeout(function(){
+        display.style.color = "red";
+        // display.style.fontSize = "30px";
+
+    },350)
+}
+setInterval("showTime()",0)
+setInterval("blink()",600)
